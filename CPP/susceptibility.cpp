ab79586c6e62c6ecbc53ecdccf59cf87431e94f1
Newtech66 <78967475+Newtech66@users.noreply.github.com>
2023-11-27 09:55:37 +0530

    Revert "data files and updates to programs"
    This reverts commit e375831d0cf58239390cf6fc36ab792a1b80266b.

--- CPP/susceptibility.cpp
+++ CPP/susceptibility.cpp
@@ -1,11 +1,8 @@
 #include "reader.h"
 #include <iostream>
-#include <iomanip>
 #include <string>
 #include <map>
 #include <cmath>
-#include <chrono>
-#include <omp.h>
 
 void analyze(const char* filename){
     std::ifstream fin(filename,std::ios_base::binary);
@@ -16,18 +13,9 @@ void analyze(const char* filename){
     std::cout << "Calculating susceptibility for " << std::string(filename) << "..." << std::endl;
     Params p = read_params(fin);
     std::map<int,double> tot, sq_tot;
-    //Start timing
-    double start = omp_get_wtime();
-    #pragma omp parallel for
     for(int s_i = 0;s_i < p.n_s * p.n_T;++s_i){
-        int T_i;
-        Grid lattice;
-        #pragma omp critical
-        {
-            auto [T,lat] = read_next(p.N, fin);
-            T_i=T;
-            lattice=lat;
-        }
+        auto [T_i,lattice] = read_next(p.N, fin);
+        double cur_T = p.T_min + T_i * (p.T_max - p.T_min) / (p.n_T - 1);
         double sum = 0.0;
         for(int i = 0;i < p.N;++i){
             for(int j = 0;j < p.N;++j){
@@ -38,32 +26,15 @@ void analyze(const char* filename){
         sq_tot[T_i] += sum * sum;
     }
     fin.close();
-    //End timing and print time taken
-    double end = omp_get_wtime();
-    const std::chrono::duration<double> time_taken{end - start};
-    const auto hrs = std::chrono::duration_cast<std::chrono::hours>(time_taken);
-    const auto mins = std::chrono::duration_cast<std::chrono::minutes>(time_taken - hrs);
-    const auto secs = std::chrono::duration_cast<std::chrono::seconds>(time_taken - hrs - mins);
-    const auto millisecs = std::chrono::duration_cast<std::chrono::milliseconds>(time_taken - hrs - mins - secs);
-    std::cout << "\nTime taken = " << hrs.count() << "h " << mins.count() << "m " << secs.count() << "s " << millisecs.count() << "ms";
-    std::cout << std::endl;
-    std::string filename_noext = std::string(filename);
-    filename_noext.erase(filename_noext.size()-4);
-    std::string outfile_name = "susceptibility_" + filename_noext + ".csv";
-    std::ofstream fout(outfile_name);
-    std::cout << "Calculated susceptibility data for " << std::string(filename);
-    std::cout << "\nWriting data to " << outfile_name << "...";
-    fout << "T,Susceptibility\n";
+    std::cout << "Calculated susceptibility data for " << std::string(filename) << ": " << std::endl;
     for(auto& [T_i,S]:tot)  S /= p.n_s;
     for(auto& [T_i,S]:sq_tot)  S /= p.n_s;
     for(auto [T_i,S1]:sq_tot){
         double cur_T = p.T_min + T_i * (p.T_max - p.T_min) / (p.n_T - 1);
         double S2 = tot[T_i];
         double X = (S1 - S2 * S2) / (2 * cur_T); //We defined k = 2 initially
-        fout << cur_T << ',' << X / (p.N * p.N) << '\n';
+        std::cout << cur_T << ' ' << X / (p.N * p.N) << '\n';
     }
-    std::cout << "\nFinished";
-    fout.close();
 }
 
 int main(int argc,char* argv[]){
